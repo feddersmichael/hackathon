b990a3aa593ad36943c94278b983149d1c35422e
deepcodin <paulhein1234@gmail.com>
2025-04-10 17:10:23 +0200

    torch in cost1

--- src/costs/b1_homogeneity.py
+++ src/costs/b1_homogeneity.py
@@ -2,7 +2,7 @@ from .base import BaseCost
 from ..data.simulation import SimulationData
 from ..data.utils import B1Calculator
 
-import numpy as np
+import torch
 
 
 class B1HomogeneityCost(BaseCost):
@@ -15,6 +15,6 @@ class B1HomogeneityCost(BaseCost):
         b1_field = self.b1_calculator(simulation_data)
         subject = simulation_data.subject
         
-        b1_field_abs = np.abs(b1_field)
+        b1_field_abs = torch.abs(b1_field)
         b1_field_subject_voxels = b1_field_abs[subject]
-        return np.mean(b1_field_subject_voxels)/np.std(b1_field_subject_voxels)
+        return torch.mean(b1_field_subject_voxels)/torch.std(b1_field_subject_voxels)
